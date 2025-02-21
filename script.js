f963b73da278578b3d436a9d2f1dd7832b0605eb
GODFREY T R <godfrey8105@gmail.com>
2025-02-21 09:49:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const apiKey = '036939265f38b7943c9d6acc5a5db0b0'; // Your API key
 
 function getWeather() {
     const location = document.getElementById('locationInput').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=" + apiKey + "&units=metric`;
 
     fetch(url)
         .then(response => response.json())
